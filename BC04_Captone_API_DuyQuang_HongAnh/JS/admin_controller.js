c0220fc59170b359e06ed4bc9c15a5a990297084
@Anh_Nguyen2604 <anieewon@gmail.com>
2022-08-29 21:55:41 -0400

    update

--- BC04_Captone_API_DuyQuang_HongAnh/JS/admin_controller.js
+++ BC04_Captone_API_DuyQuang_HongAnh/JS/admin_controller.js
@@ -1,16 +1,17 @@
-/*const emptycart = `<span class="empty-cart"
->Looks Like You Haven't Added Any Product In The Cart</span
->`;
-//turn on loading
-export let batLoading = () => {
-  document.getElementById("loading").style.display = "flex";
-};
-//turn off Loading
-export let tatLoading = () => {
-  document.getElementById("loading").style.display = "none";
-};*/
+// {/*const emptycart = `<span class="empty-cart"
+// >Looks Like You Haven't Added Any Product In The Cart</span
+// >`;
+// //turn on loading
+// export let batLoading = () => {
+//   document.getElementById("loading").style.display = "flex";
+// };
+// //turn off Loading
+// export let tatLoading = () => {
+//   document.getElementById("loading").style.display = "none";
+// };*/}
 
-import { validation } from "./admin_validation";
+import { dataPhone } from "./admin_model.js";
+import { validation } from "./admin_validation.js";
 
 export const renderDSDT = (listDT) => {
   let contentHTML = "";
@@ -20,7 +21,7 @@ export const renderDSDT = (listDT) => {
     <td>${dt.id}</td>
     <td>${dt.name}</td>
     <td>${dt.price}</td>
-    <td><img src="${dt.img}" alt=""></td>
+    <td class="text-center"><img class="w-25" src="${dt.img}" alt=""></td>
     <td>${dt.desc}</td>
     <td>
     <button class="btn btn-danger" onclick="xoaDienThoai('${dt.id}')"> Delete</button>
@@ -31,48 +32,70 @@ export const renderDSDT = (listDT) => {
     </tr>`;
     contentHTML += contentTr;
   });
-  document.getElementById("tblDanhSachSP");
+  document.getElementById("tblDanhSachSP").innerHTML = contentHTML;
 };
 
-export const getInfoForm = () => {
-  const id = document.getElementById("numberSP").value;
+// {// export const layThongTinTuForm = () => {
+// //   const id = document.getElementById("numberSP").value;
+// //   const name = document.getElementById("TenSP").value;
+// //   const price = document.getElementById("GiaSP").value;
+// //   const screen = document.getElementById("manhinhSP").value;
+// //   const backCam = document.getElementById("back_camera").value;
+// //   const frontCam = document.getElementById("front_camera").value;
+// //   const image = document.getElementById("HinhSP").value;
+// //   const desc = document.getElementById("MoTa").value;
+// //   const type = document.getElementById("loaiSP").value;
+
+// //   return new dataPhone(
+// //     id,
+// //     name,
+// //     price,
+// //     screen,
+// //     backCam,
+// //     frontCam,
+// //     image,
+// //     desc,
+// //     type
+// //   );
+// // };
+// }
+export const layThongTinTuForm = () => {
+  // const id = document.getElementById("numberSP").value;
   const name = document.getElementById("TenSP").value;
   const price = document.getElementById("GiaSP").value;
   const screen = document.getElementById("manhinhSP").value;
-  const backCam = document.getElementById("back_camera").value;
-  const frontCam = document.getElementById("front_camera").value;
-  const image = document.getElementById("HinhSP").value;
+  const backCamera = document.getElementById("back_camera").value;
+  const frontCamera = document.getElementById("front_camera").value;
+  // const image = document.getElementById("HinhSP").value;
   const desc = document.getElementById("MoTa").value;
   const type = document.getElementById("loaiSP").value;
 
-  return new DienThoai(
-    id,
+  return new dataPhone(
     name,
     price,
     screen,
-    backCam,
-    frontCam,
-    image,
+    backCamera,
+    frontCamera,
     desc,
     type
   );
 };
 
-export const showInfoForm = (dt) => {
-  document.getElementById("numberSP").value = dt.id;
-  document.getElementById("TenSP").value = dt.name;
-  document.getElementById("GiaSP").value = dt.price;
-  document.getElementById("manhinhSP").value = dt.screen;
-  document.getElementById("back_camera").value = dt.backCam;
-  document.getElementById("front_camera").value = dt.frontCam;
-  document.getElementById("HinhSP").value = dt.image;
-  document.getElementById("MoTa").value = dt.desc;
-  document.getElementById("loaiSP").value = dt.type;
+export const showThongTinLenForm = (dthoai) => {
+  document.getElementById("numberSP").value = dthoai.id;
+  document.getElementById("TenSP").value = dthoai.name;
+  document.getElementById("GiaSP").value = dthoai.price;
+  document.getElementById("manhinhSP").value = dthoai.screen;
+  document.getElementById("back_camera").value = dthoai.backCam;
+  document.getElementById("front_camera").value = dthoai.frontCam;
+  // document.getElementById("HinhSP").value = dthoai.image;
+  document.getElementById("MoTa").value = dthoai.desc;
+  document.getElementById("loaiSP").value = dthoai.type;
 };
 
 export const verifyValidation = () => {
-  let newDT = getInfoForm();
-  let isValide =
+  let newDT = layThongTinTuForm();
+  let isValidTen =
     validation.kiemTraRong(
       newDT.name,
       "nameSP",
@@ -97,7 +120,54 @@ export const verifyValidation = () => {
       newDT.price,
       "priceSP",
       "gia item khong hop le",
-      1000000,
+      10,
       20000000
     );
+
+  let isValidManHinh = validation.kiemTraRong(
+    newDT.screen,
+    "screenSP",
+    "Man hinh ko duoc de rong"
+  );
+
+  let isValidFrontCam = validation.kiemTraRong(
+    newDT.frontCamera,
+    "frontSP",
+    "Camera truoc ko duoc de rong"
+  );
+  let isValidbackCam = validation.kiemTraRong(
+    newDT.backCamera,
+    "backSP",
+    "Camera sau ko duoc de rong"
+  );
+  let isValidDescription = validation.kiemTraRong(
+    newDT.desc,
+    "descSP",
+    "Mo ta  ko duoc de rong"
+  );
+
+  let isValidType = validation.kiemTraRong(
+    newDT.type,
+    "typeSP",
+    "Loai dt ko duoc de rong"
+  );
+
+  let isValid =
+    isValidTen &
+    isValidPrice &
+    isValidManHinh &
+    isValidFrontCam &
+    isValidbackCam &
+    isValidDescription &
+    isValidType;
+  return isValid;
+};
+export const removeSpancontent = () => {
+  document.getElementById("nameSP").innerText = "";
+  document.getElementById("priceSP").innerText = "";
+  document.getElementById("screenSP").innerText = "";
+  document.getElementById("frontSP").innerText = "";
+  document.getElementById("backSP").innerText = "";
+  document.getElementById("descSP").innerText = "";
+  document.getElementById("typeSP").innerText = "";
 };
--- BC04_Captone_API_DuyQuang_HongAnh/JS/admin_main.js
+++ BC04_Captone_API_DuyQuang_HongAnh/JS/admin_main.js
@@ -1,86 +1,211 @@
 import {
   verifyValidation,
-  getInfoForm,
-  showInfoForm,
+  layThongTinTuForm,
+  showThongTinLenForm,
   renderDSDT,
-} from "./admin_controller";
-import { getInfoForm, verifyValidation } from "./admin_controller";
-const BASE_URL = "https://62f99cb8e056448035383654.mockapi.io/";
+} from "./admin_controller.js";
+// {
+// /*
+// // let getDSDT = () => {
+// //   batLoading();
+// //   axios({
+// //     url: `${BASE_URL}/phone`,
+// //     method: "GET",
+// //   })
+// //     .then(function (res) {
+// //       tatLoading();
+// //       renderDSDT(res.data);
+// //     })
+// //     .catch(function (err) {
+// //       tatLoading();
+// //     });
+// // };
+
+// // getDSDT();
+// // document.getElementById("add_product").style.display = "block";
+// // document.getElementById("update_product").style.display = "none";
+
+// document.getElementById("add_product").addEventListener("click", add_product);
+
+// //btn addproduct
+// function add_product() {
+//   console.log("sunny");
+//   // let newProduct = getInfoForm();
+//   // if (verifyValidation()) {
+//   //   batLoading();
+//   //   axios({
+//   //     url: `${BASE_URL}/phone`,
+//   //     method: "GET",
+//   //     data: newProduct,
+//   //   })
+//   //     .then(function (res) {
+//   //       tatLoading();
+//   //       renderDSDT(res.data);
+//   //     })
+//   //     .catch(function (err) {
+//   //       tatLoading();
+//   //     });
+//   // }
+// }
+// // window.addProduct = addProduct;
+
+// // btn updatePRoduc
+
+// // xoa Dien Thoai
+// // const deletePro = (id) => {
+// //   batLoading();
+// //   axios({
+// //     url: `${BASE_URL}/phone/${id}`,
+// //     method: "GET",
+// //   })
+// //     .then(function (res) {
+// //       tatLoading();
+// //       renderDSDT(res.data);
+// //     })
+// //     .catch(function (err) {
+// //       tatLoading();
+// //     });
+// // };
+// // window.deletePro = deletePro;
+
+// // //sua Dien Thoai
+// // const modifyPro = (id) => {
+// //   removeInfo();
+// //   axios({
+// //     url: `${BASE_URL}/phone/${id}`,
+// //     method: "GET",
+// //   })
+// //     .then(function (res) {
+// //       tatLoading();
+// //       showInfoForm(res.data);
+// //     })
+// //     .catch(function (err) {
+// //       tatLoading();
+// //     });
+// //   document.getElementById("add_product").style.display = "none";
+// //   document.getElementById("update_product").style.display = "block";
+// // };
+// // window.modifyPro = modifyPro;
+// */}
+
+const BASE_URL = "https://62f99cb8e056448035383654.mockapi.io";
+// {/*
+// var batLoading = function () {
+//   document.getElementById("loading").style.display = "flex";
+// };
+// var tatLoading = function () {
+//   document.getElementById("loading").style.display = "none";
+// };*/}
+
+export let batLoading = () => {
+  document.getElementById("loading").style.display = "flex";
+};
+
+export let tatLoading = () => {
+  document.getElementById("loading").style.display = "none";
+};
 
 let getDSDT = () => {
   batLoading();
   axios({
-    url: `${BASE_URL}/phone`,
+    url: `${BASE_URL}/capstoneapi`,
     method: "GET",
   })
     .then(function (res) {
+      // console.log(res.data);
       tatLoading();
       renderDSDT(res.data);
     })
     .catch(function (err) {
       tatLoading();
+      console.log(err);
     });
 };
-
 getDSDT();
-document.getElementById("add_product").style.display = "block";
-document.getElementById("update_product").style.display = "none";
 
-//btn addproduct
-const addProduct = () => {
-  let newProduct = getInfoForm();
+document.getElementById("btnThemSP").addEventListener("click", () => {
+  document.getElementById("add_product").style.display = "inline-block";
+});
+
+//xoá sản phẩm
+function xoaDienThoai(id) {
+  axios({
+    url: `${BASE_URL}/capstoneapi/${id}`,
+    method: "DELETE",
+  })
+    .then(function (res) {
+      getDSDT();
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+window.xoaDienThoai = xoaDienThoai;
+
+// thêm sản phẩm
+function addProduct() {
+  document.getElementById("add_product").removeAttribute("data-dismiss");
+
+  var newDT = layThongTinTuForm();
+
   if (verifyValidation()) {
+    document
+      .getElementById("add_product")
+      .setAttribute("data-dismiss", "modal");
+
     batLoading();
     axios({
-      url: `${BASE_URL}/phone`,
-      method: "GET",
-      data: newProduct,
+      url: `${BASE_URL}/capstoneapi`,
+      method: "POST",
+      data: newDT,
     })
       .then(function (res) {
         tatLoading();
-        renderDSDT(res.data);
+        getDSDT();
       })
       .catch(function (err) {
         tatLoading();
+        console.log(err);
       });
   }
-};
+}
 window.addProduct = addProduct;
 
-// btn updatePRoduc
-
-// xoa Dien Thoai
-const deletePro = (id) => {
+// sửa điện thoại
+function suaDienThoai(id) {
   batLoading();
   axios({
-    url: `${BASE_URL}/phone/${id}`,
+    url: `${BASE_URL}/capstoneapi/${id}`,
     method: "GET",
   })
     .then(function (res) {
       tatLoading();
-      renderDSDT(res.data);
+      showThongTinLenForm(res.data);
+      console.log(res.data);
     })
     .catch(function (err) {
       tatLoading();
     });
-};
-window.deletePro = deletePro;
+}
+window.suaDienThoai = suaDienThoai;
 
-//sua Dien Thoai
-const modifyPro = (id) => {
-  removeInfo();
+function updateProduct(id) {
+  var newDT = layThongTinTuForm();
+  var id = newDT.id;
+  console.log(newDT);
+  batLoading();
   axios({
-    url: `${BASE_URL}/phone/${id}`,
-    method: "GET",
+    url: `${BASE_URL}/capstoneapi/${id}`,
+    method: "PUT",
+    data: newDT,
   })
     .then(function (res) {
       tatLoading();
-      showInfoForm(res.data);
+      getDSDT();
     })
     .catch(function (err) {
       tatLoading();
+      console.log(err);
     });
-  document.getElementById("add_product").style.display = "none";
-  document.getElementById("update_product").style.display = "block";
-};
-window.modifyPro = modifyPro;
+}
+window.updateProduct = updateProduct;
